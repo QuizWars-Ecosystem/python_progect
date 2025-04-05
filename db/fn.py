872a52f38af4840511acdd463fcd7df71d34f738
Alex <aksenov.houme@gmail.com>
2025-04-05 15:02:39 +0300

    project structure

--- db/fn.py
+++ db/fn.py
@@ -3,10 +3,6 @@ from database import async_engine, async_session_factory, Base
 
 async def create_tables():
     async with async_engine.begin() as conn:
-        conn.run_sync(Base.metadata.drop_all())
-        conn.run_sync(Base.metadata.create_all())
-
-
-
-
+        await conn.run_sync(Base.metadata.drop_all())
+        await conn.run_sync(Base.metadata.create_all())
 
