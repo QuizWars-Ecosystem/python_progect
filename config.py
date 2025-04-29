322c898791bb8474a57343796d67f3f319cc6e37
Alex <aksenov.houme@gmail.com>
2025-04-29 15:10:55 +0300

    yaml configuration file

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
+import yaml
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 from pathlib import Path
-env_path = Path(__file__).parent / ".env"
+
+yaml_path = Path(__file__).parent / "config.yaml"
 
 class RunConfig(BaseModel):
     host: str = "127.0.0.1"
     port: int = 8080
 
-class Database(BaseSettings):
+class Database(BaseModel):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -22,13 +24,16 @@ class Database(BaseSettings):
     def DB_URL(self):
         return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
 
-    model_config = SettingsConfigDict(env_file=env_path)
 
 class Settings(BaseSettings):
-    run: RunConfig = RunConfig()
-    db: Database = Database()
+    run: RunConfig
+    db: Database
 
-    model_config = SettingsConfigDict(env_file=env_path)
+    @classmethod
+    def from_yaml(cls, path: Path = yaml_path):
+        with open(path, "r") as f:
+            config_data = yaml.safe_load(f)
+        return cls(**config_data)
 
-settings = Settings()
+settings = Settings.from_yaml()
 
